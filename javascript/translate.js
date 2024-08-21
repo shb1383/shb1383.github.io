e81a1bb5125457d47eb22a3a65a78921b3b92578
syrene <144994323+shb1383@users.noreply.github.com>
2024-08-21 21:21:36 +0900

    added footer translation to translation function

--- javascript/translate.js
+++ javascript/translate.js
@@ -1,10 +1,14 @@
+// Get the nav elements
 var projectsNav = document.getElementById("projectsNav");
 var skillsNav = document.getElementById("skillsNav");
 var servicesNav = document.getElementById("servicesNav");
 var contactNav = document.getElementById("contactNav");
 var translate = document.getElementById("translate");
 
+// Get the footer location
+var locationFooter = document.querySelector('.location');
 
+// Toggle the styling between japanese and english when the translate nav element is selected
 translate.addEventListener('click', () => {
   if (translate.innerHTML === "日本語") {
     projectsNav.innerHTML = "プロジェクト";
@@ -12,6 +16,7 @@ translate.addEventListener('click', () => {
     servicesNav.innerHTML = "サービス";
     contactNav.innerHTML = "コンタクト";
     translate.innerHTML = "ENGLISH";
+    locationFooter.innerHTML = '<p><i class="fa-solid fa-location-dot"></i> 東京</p>';
   }
   else {
     projectsNav.innerHTML = "PROJECTS";
@@ -19,5 +24,6 @@ translate.addEventListener('click', () => {
     servicesNav.innerHTML = "SERVICES";
     contactNav.innerHTML = "CONTACT";
     translate.innerHTML = "日本語";
+    locationFooter.innerHTML = '<p><i class="fa-solid fa-location-dot"></i> Tokyo, Japan</p>';
   }
 });
