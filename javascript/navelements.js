977961366fa2266659f04ddf910a348d40ef3b4c
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-02 21:16:15 +0900

    cleaned and removed unnecessary functions and styling

--- javascript/navelements.js
+++ javascript/navelements.js
@@ -33,32 +33,22 @@ var body = document.querySelector('body');
 // Get the mobile menu container
 var mobileMenuContainer = document.querySelector('.mobile_menu_container');
 
-// Get the hero link
-var heroLink = document.getElementById('hero_nav');
-
-// Get the translate link
-var translateLink = document.getElementById('translate');
-
-// Changes mobile menu container height to 0, icon to "bars", displays hero and translate nav links, and enables body scrolling after selecting navbar link
+// Changes mobile menu container height to 0, icon to "bars", and enables body scrolling after selecting navbar link
 navElements.forEach(navElement => {
   navElement.addEventListener('click', () => {
     document.querySelector('.icon').innerHTML = '<i class="fa-solid fa-bars"></i>';
     body.style.height = 'auto';
     body.style.overflow = 'auto';
     mobileMenuContainer.style.height = 0;
-    heroLink.style.display = "inline";
-    translateLink.style.display = "inline";
   });
 });
 
-// Changes mobile menu container height to 0, icon to "bars", displays hero and translate nav links, and enables body scrolling after clicking outside of mobile menu
+// Changes mobile menu container height to 0, icon to "bars", and enables body scrolling after clicking outside of mobile menu
 sections.forEach(section => {
   section.addEventListener('click', () => {
     document.querySelector('.icon').innerHTML = '<i class="fa-solid fa-bars"></i>';
     body.style.height = 'auto';
     body.style.overflow = 'auto';
     mobileMenuContainer.style.height = 0;
-    heroLink.style.display = "inline";
-    translateLink.style.display = "inline";
   });
 });
