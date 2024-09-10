3edce60b883c653f59d233b567df63556af3fbaf
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-10 18:39:30 +0900

    updated styling for footer links hover in light and dark mode

--- javascript/mode.js
+++ javascript/mode.js
@@ -295,12 +295,12 @@ mode.addEventListener('click', () => {
 });
 
 mobileMode.addEventListener('click', () => {
-  if (contactEmail.classList.contains('contact_light')) {
-    contactEmail.classList.remove('contact_light');
-    contactEmail.classList.add('contact_dark');
+  if (contactEmail.classList.contains('contact_email_light')) {
+    contactEmail.classList.remove('contact_email_light');
+    contactEmail.classList.add('contact_email_dark');
   } else {
-    contactEmail.classList.remove('contact_dark');
-    contactEmail.classList.add('contact_light');
+    contactEmail.classList.remove('contact_email_dark');
+    contactEmail.classList.add('contact_email_light');
   }
 });
 
